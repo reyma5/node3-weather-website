133a785fc347e2ef32c9cfeb4462c82e361bf862
reyma5 <reymalitdog5@gmail.com>
2021-08-30 17:57:29 +0800

    Additional info forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -24,7 +24,7 @@
 // module.exports = forecast;
 
 // ---- 40. DESTRUCTURING AND PROPERTY SHORTHAND CHALLENGE ----//
-
+/*
 const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
@@ -43,6 +43,36 @@ const forecast = (latitude, longitude, callback) => {
           " degrees out. It feels like " +
           body.current.feelslike +
           " degrees out."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
+*/
+
+// -------------- 68. NEW FEATURE DEPLOYMENT WORKFLOW --------------//
+
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?key=value&access_key=341446ed83e1a71a76c053e1c3a38359&query=${latitude}, + ${longitude}&units=f`;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!!", undefined);
+    } else if (body.error) {
+      callback(body.error.info, undefined);
+    } else {
+      callback(
+        undefined,
+        body.current.weather_descriptions[0] +
+          ". It is currently " +
+          body.current.temperature +
+          " degrees out. It feels like " +
+          body.current.feelslike +
+          " degrees out. The humidity is " +
+          body.current.humidity +
+          " percent."
       );
     }
   });
